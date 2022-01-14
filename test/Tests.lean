b8b070201affd4473ec0288704def8f73d4a66f7
Anders Christiansen Sørby <anders@sorby.xyz>
2022-01-14 15:21:30 +0100

    Add bitmap test

--- test/Tests.lean
+++ test/Tests.lean
@@ -1,22 +1,24 @@
 import SDL
 
+def bitmapTest : IO Unit := do
+  let r ← SDL.init
+  if r != 0 then
+    IO.eprintln "Error in init"
+  let window ← SDL.createWindow "Test" 800 500
+  let renderer ← SDL.createRenderer window
+  let surf ← SDL.loadBMP "images/green_nebula.bmp"
+  let tex ← SDL.createTextureFromSurface renderer surf
+  SDL.renderCopy renderer tex
+  SDL.renderPresent renderer
+  SDL.delay 5000
+  SDL.destroyWindow window
+  SDL.quit
+
 def main (args : List String) : IO UInt32 := do
   try
-    let r ← SDL.init
-    if r != 0 then
-      IO.eprintln "Error in init"
-    let window ← SDL.createWindow "Test" 800 500
-    let renderer ← SDL.createRenderer window
-    let surf ← SDL.loadBMP "test/pixel-world-map.bmp"
-    let tex ← SDL.createTextureFromSurface renderer surf
-    SDL.renderCopy renderer tex
-    SDL.renderPresent renderer
-    SDL.delay 5000
-    SDL.destroyWindow window
-    SDL.quit
+    bitmapTest
     pure 0
   catch e =>
     IO.eprintln <| "error: " ++ toString e
     IO.eprintln (← SDL.getError)
     pure 1
-
